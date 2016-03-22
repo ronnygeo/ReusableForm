1ccc2221e4aa8276d3d4d180fd8867e4bae2872b
ronnygeo <ronnygeomatt@gmail.com>
2016-03-22 13:33:03 -0400

    Working basic functionality.

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@
  */
 
 $(function(){
-    var data = ['data','j','k'];
+    var data = ['s','s','s'];
     var form = $('#myForm');
     var table = $(form).find('table');
     var inputField = $(form).find('thead').find('input');
@@ -13,32 +13,32 @@ $(function(){
     function repopulateList(){
         $(table).find('tbody').empty();
         for (var i in data) {
-            $(table).find('tbody').append('<tr class="dataRow"><td>'+data[i]+'</td><td><a>Delete</a></td></tr>');
+            // var trow =
+            $(table).find('tbody').append('<tr class="dataRow"><td>'+data[i]+'</td><td><a class="deleteButton" data-row="'+i+'">Delete</a></td></tr>');
         }
     }
     repopulateList();
 
     //Getting the input from the input field and adding it to the data array.
-    $('#addToListButton').bind("click", (function () {
+    $('#addToListButton').on("click", (function (e) {
+        e.preventDefault();
         var item = inputField.val();
         data.push(item);
+        $(inputField).val("");
         repopulateList();
     }));
 
-    // $(table).find('tr').click( function(e){
-    //     console.log(e)
-    //     alert('You clicked row '+ ($(this).index()) );
-    // });
-
-    $(table).find('tbody').find('a').click(function () {
-        var row = $(this).closest('tr');
-        //console.log($('tbody').index(row))
-        var index = $(row).index() - 1;
-        console.log($(this).closest('tr'));
-        console.log($(this).index());
-        //console.log(data[index]);
-        data.splice(index, 1);
-        repopulateList();
+    $('#formTableBody').on('mouseenter', function () {
+        $(this).children().each(function () {
+        // console.log($(this));
+        $(this).find('.deleteButton').on("click", function (e) {
+            e.preventDefault();
+            var rowId = $(this).data("row");
+            data.splice(rowId,1);
+            repopulateList();
+            e.stopPropagation();
+        });
+    });
     });
 
 
